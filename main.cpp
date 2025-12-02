50d544b9bddae38c2cf0767fd054602eb83d563c
monukoni <kartoplya213@gmail.com>
2025-12-02 13:23:56 +0200

    lab6

--- main.cpp
+++ main.cpp
@@ -1,49 +1,102 @@
 #include <iostream>
-#include <string>
-#include <syncstream>
-#include <chrono>
-#include <future>
+#include <queue>
+#include <random>
+#include <coroutine>
+#include <memory>
 
-using namespace std;
+std::queue<int> numbers;
 
-void work_func(const string& name, int seconds) {
-    this_thread::sleep_for(chrono::seconds(seconds));
-    osyncstream(cout) << name << endl;
+struct SuspendIfOdd {
+    int value;
+
+    bool await_ready() const {
+        return (value % 2 == 0);
+    }
+
+    void await_suspend(std::coroutine_handle<>) const {
+        std::cout << "  -> [AWAITER] Odd number detected (" << value << "). Suspending coroutine...\n";
+    }
+
+    void await_resume() const {}
+};
+
+struct Generator {
+    struct promise_type {
+        Generator get_return_object() {
+            return Generator{std::coroutine_handle<promise_type>::from_promise(*this)};
+        }
+        std::suspend_always initial_suspend() { return {}; } // Зупинка на старті
+        std::suspend_always final_suspend() noexcept { return {}; }
+        void return_void() {}
+        void unhandled_exception() { std::terminate(); }
+    };
+
+    std::coroutine_handle<promise_type> handle;
+
+    Generator(std::coroutine_handle<promise_type> h) : handle(h) {}
+    ~Generator() { if (handle) handle.destroy(); }
+
+    void resume() {
+        if (handle && !handle.done()) handle.resume();
+    }
+
+    bool done() const {
+        return !handle || handle.done();
+    }
+};
+
+
+Generator randomNumberGenerator() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(1, 256);
+
+    while (true) {
+        int random_number = distrib(gen);
+
+        std::cout << "Generated: " << random_number << "\n";
+        numbers.push(random_number);
+
+
+        co_await SuspendIfOdd{random_number};
+    }
 }
 
-void work() {
-    auto start_time = chrono::high_resolution_clock::now();
-
-    auto f_A2 = std::async(std::launch::async, work_func, "A2", 7);
-
-    work_func("A1", 7);
-
-    f_A2.wait();
-
-    auto f_B2 = std::async(std::launch::async, work_func, "B2", 1);
-    auto f_B3 = std::async(std::launch::async, work_func, "B3", 1);
-
-    work_func("B1", 1);
-
-    f_B2.wait();
-    f_B3.wait();
-
-    auto f_D = std::async(std::launch::async, work_func, "D", 1);
-
-    work_func("C", 1);
-
-    f_D.wait();
-
-    auto end_time = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration<double>(end_time - start_time).count();
-
-    cout << duration << endl;
-    cout << "Work is done!" << endl;
+void print_queue() {
+    std::queue<int> temp = numbers;
+    std::cout << "Current queue: [ ";
+    while (!temp.empty()) {
+        std::cout << temp.front() << " ";
+        temp.pop();
+    }
+    std::cout << "]\n";
 }
 
 int main() {
-    work();
+    auto gen = randomNumberGenerator();
+
+    std::cout << "--- Coroutine Demo Started ---\n";
+    std::cout << "Type 'next' to generate until next odd number.\n";
+    std::cout << "Type 'exit' to quit.\n\n";
+
+    std::string command;
+    while (true) {
+        std::cout << "> Command: ";
+        std::cin >> command;
+
+        if (command == "next") {
+            std::cout << "Resuming coroutine...\n";
+            gen.resume();
+            print_queue();
+            std::cout << "-----------------------------\n";
+        }
+        else if (command == "exit") {
+            break;
+        }
+        else {
+            std::cout << "Unknown command.\n";
+        }
+    }
+
     return 0;
 }
-
-
